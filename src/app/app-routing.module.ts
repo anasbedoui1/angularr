667d574b205d980cf458fbad6542438b482931c7
anas <bedouianas@kef.r-iset.tn>
2024-07-01 23:55:56 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+// app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LouageListComponent } from './louage-list/louage-list.component';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
+import { UserListComponent } from './user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'louages', component: LouageListComponent },
+  { path: 'reservations', component: ReservationListComponent },
+  { path: 'users', component: UserListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LouageListComponent } from './louage-list/louage-list.component';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LouageListComponent, ReservationListComponent, UserListComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
